92f13dbb483e0e095828fe81d030f76f63ff8267
Brian Evans <dhwyll@gmail.com>
2017-09-30 14:15:18 -0700

    Initial commit

---
+++ routes/api-routes.js
@@ -0,0 +1,17 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// This data source holds an array of information on the potential matches.
+// ===============================================================================
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+	
+	app.get("/", function(req, res) {
+		res.render("index");
+	});
+};
