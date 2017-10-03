84ce7c69fe87828b0e427c3daca6a39e36cfc746
Julie Ching <juliemching@gmail.com>
2017-10-03 11:14:18 -0700

    update to Dev

--- server.js
+++ server.js
@@ -16,13 +16,13 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
-var db = require( "./models" );
+var db = require( './models' );
 
 // Sets up the Express app to handle data parsing
 app.use( bodyParser.json() );
-app.use( bodyParser.urlencoded( { "extended": true } ) );
+app.use( bodyParser.urlencoded( { 'extended': true } ) );
 app.use( bodyParser.text() );
-app.use( bodyParser.json( { "type": "application/vnd.api+json" } ) );
+app.use( bodyParser.json( { 'type': 'application/vnd.api+json' } ) );
 
 // Static directory
 app.use( express.static( 'public' ) );
@@ -130,12 +130,12 @@ app.use( function ( req, res, next ) {
 // Set handlebars.
 var exphbs = require( 'express-handlebars' );
 
-app.engine( "handlebars", exphbs( { "defaultLayout": "main" } ) );
-app.set( "view engine", "handlebars" );
+app.engine( 'handlebars', exphbs( { 'defaultLayout': 'main' } ) );
+app.set( 'view engine', 'handlebars' );
 
 // Routes
 // =============================================================
-require( "./routes/api-routes.js" )( app );
+require( './routes/api-routes.js' )( app );
 
 // Syncing our sequelize models and then starting our express app
 db.sequelize.sync( ).then( function () {
@@ -143,20 +143,3 @@ db.sequelize.sync( ).then( function () {
         console.log( 'App listening on PORT ' + PORT );
     } );
 } );
-
-if(env === "development") {
-	// Syncing our sequelize models and then starting our express app
-	db.sequelize.sync( { "force": true } ).then( function () {
-		app.listen( PORT, function () {
-			console.log( "App listening on PORT " + PORT );
-		} );
-	} );
-} else {
-	// Syncing our sequelize models and then starting our express app
-	db.sequelize.sync( { "force": false } ).then( function () {
-		app.listen( PORT, function () {
-			console.log( "App listening on PORT " + PORT );
-		} );
-	} );
-}
-
